8882c96fc136055ca0e8f79d6676a97e3f494907
nitin yadav <nitinyadav484220@gmail.com>
2025-11-12 00:18:36 +0530

    nitin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -130,7 +130,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key']
 }));
-
+  
   // Compression
   app.use(compression());
 
